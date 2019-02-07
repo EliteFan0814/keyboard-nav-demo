ca8742af89e0dec09ed241345c4cc6f9aa08b4af
RK <rkfecode@gmail.com>
2019-02-07 10:54:03 +0800

    .

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 var keys = {
-    0: ['Esc','1', '2', '3', '4', '5', '6', '7', '8', '9', '0','-','+','~'],
-    1: ['Tab','q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p','[',']','Delete'],
-    2: ['Control','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',';','"','Return'],
-    3: ['shl','z', 'x', 'c', 'v', 'b', 'n', 'm','<','>','?','shr','fn'],
+    0: ['Esc', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', '~'],
+    1: ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'Delete'],
+    2: ['Control', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', 'Return'],
+    3: ['shl', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', 'shr', 'fn'],
     length: 4
 }
 
@@ -20,15 +20,15 @@ var hash = {
     p: 'paypal.com'
 }
 
- //取出localstorage 中的 storage1 對應的 hash
- var hashInLocalStorage = JSON.parse(localStorage.getItem('storage1') || 'null')
+//取出localstorage 中的 storage1 對應的 hash
+var hashInLocalStorage = JSON.parse(localStorage.getItem('storage1') || 'null')
 if (hashInLocalStorage) {
     hash = hashInLocalStorage
 }
 
 
 //根據hash建立鍵盤
-index = 0
+var index = 0
 while (index < keys.length) {
     div1 = document.createElement('div')
     main1.appendChild(div1)
@@ -36,20 +36,33 @@ while (index < keys.length) {
     index2 = 0
     while (index2 < row.length) {
         kbd1 = document.createElement('kbd')
-        div1.appendChild(kbd1)
-        kbd1.textContent = row[index2]
+        span1 = document.createElement('span')
+        span1.textContent = row[index2]
+        span1.className = 'text'
         kbd1.className = 'key'
         kbd1.id = row[index2] + 'p'
         button1 = document.createElement('button')
-        kbd1.appendChild(button1)
         button1.textContent = 'edit'
         button1.id = row[index2]
+        img1 = document.createElement('img')
+        if (hash[row[index2]]) {
+            img1.src = 'https://' + hash[row[index2]] + '/favicon.ico'
+        }else{
+            img1.src = 'https://i.loli.net/2019/02/07/5c5b9d224db60.png'
+        }
+        img1.onerror = function (xxx) {
+            xxx.target.src = 'https://i.loli.net/2019/02/07/5c5b9d224db60.png'
+        }
         button1.onclick = function (click1) {
             key = click1.target.id
             x = prompt('input new link')
             hash[key] = x
             localStorage.setItem('storage1', JSON.stringify(hash))
         }
+        kbd1.appendChild(span1)
+        kbd1.appendChild(img1)
+        div1.appendChild(kbd1)
+        kbd1.appendChild(button1)
         index2 = index2 + 1
     }
     index = index + 1
@@ -70,21 +83,21 @@ fn.parentElement.className = 'key fn second-keycap';
 
 
 
-
+// 監聽鍵盤
 document.onkeypress = function (press1) {
     key = press1.key
     website = hash[key]
     document.getElementById('p1').innerHTML = website
-    }
+}
 
 document.onkeydown = function (pp) {
     presedKey = pp.key
     console.log(presedKey)
-   document.getElementsByClassName('key').classList.add('pressed')
+    document.getElementsByClassName('key').classList.add('pressed')
 }
 
 document.onkeyup = function (ee) {
     presedKey1 = ee.key
     console.log(presedKey1)
-   document.getElementsByClassName('key').classList.remove('pressed')
+    document.getElementsByClassName('key').classList.remove('pressed')
 }
